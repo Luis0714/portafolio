354004573976d2347772329edb796e227809bb1d
Luis Andres Martinez Estremor <luis.martineze@opitech.com.co>
2024-09-06 17:08:25 -0500

    refactor: Update image paths for Projects component

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
