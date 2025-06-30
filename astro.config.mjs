a9071616703b8d2de6061d69fd2a2d1bda70b0a6
Luis Andres Martinez Estremor <luian.me0714@gmail.com>
2025-06-29 22:31:05 -0500

    Add social media icons and update layout with new favicon and Open Graph metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
+  devToolbar: {
+    enabled: false,
+  },
 });
