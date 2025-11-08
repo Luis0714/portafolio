35a4320fa21a4ea72cf456e0608082c40a947781
Luis Andres Martinez Estremor <luian.me0714@gmail.com>
2025-11-08 17:04:27 -0500

    feat: update Vercel adapter configuration and add runtime fix script for Node.js 20.x

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,9 @@ export default defineConfig({
     }),
   ],
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    functionPerRoute: false,
+  }),
   devToolbar: {
     enabled: false,
   },
