4249a2e81a8e6b32043f17624139d3ac60d24600
Luis Andres Martinez Estremor <luian.me0714@gmail.com>
2025-11-08 16:34:39 -0500

    feat: add sitemap integration, update site metadata, and enhance layout with theme toggle functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://estremor.com",
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: "monthly",
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
   output: "server",
   adapter: vercel(),
   devToolbar: {
